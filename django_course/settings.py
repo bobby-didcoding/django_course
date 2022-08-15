603d06f0f87fc4d72db60c24969048d8672c9411
bobby-didcoding <bobby@didcoding.com>
2022-08-15 11:06:40 +0100

    module_8 debugged

--- django_course/settings.py
+++ django_course/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'django_course.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
