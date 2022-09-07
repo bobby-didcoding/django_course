0f3c46827bc7b55853af614e2b7b7bbaf0dcce99
Bobby Stearman <80459294+bobby-didcoding@users.noreply.github.com>
2022-09-07 14:29:43 +0100

    Update views.py
    Updated UserInfoView

--- users/views.py
+++ users/views.py
@@ -84,7 +84,7 @@ def UserInfoView(request):
     context = {'form': u_form}
 
     if request.method == "POST":
-        form = UserProfileForm(instance = user, data = request.POST)
+        form = UserAlterationForm(instance = user, data = request.POST)
         if form.is_valid:
             form.save()
             return redirect('/user-info/')
